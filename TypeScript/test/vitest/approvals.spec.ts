76aeb2a579affd4ecb89c4219d284985779d5fdf
hypothete <erin@hypothete.com>
2026-04-19 17:08:04 -0700

    swapped out updateQuality method and implemented conjured handling

--- TypeScript/test/vitest/approvals.spec.ts
+++ TypeScript/test/vitest/approvals.spec.ts
@@ -12,12 +12,6 @@ import { Item, GildedRose } from '@/gilded-rose';
  */
 
 describe('Gilded Rose Approval', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
-
-    expect(items).toMatchSnapshot();
-  });
 
   it('should thirtyDays', () => {
     const consoleOutput = execSync(
